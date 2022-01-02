407ffc6812f3a92485812688bcb1227689ed78e9
Star <star@gmail.com>
2022-01-02 22:45:57 +0500

    done

--- server.js
+++ server.js
@@ -2,38 +2,84 @@ var express = require('express');
 var app = express();
 var multer = require('multer')
 var cors = require('cors');
+var fs = require('fs');
+var videoshow = require('videoshow')
+var _ = require('lodash')
+app.use(express.json());
 app.use(cors())
+
+const images = []
+const audio = []
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'public')
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now() + '-' +file.originalname )
+  destination: function (req, file, cb) {
+
+    if (file.fieldname === 'images') {
+      images.push({ path: './public/images/' + file.originalname })
+      cb(null, 'public/images')
+    } else if (file.fieldname === 'audio') {
+      audio.push({ path: './public/audio/' + file.originalname })
+      cb(null, 'public/audio')
     }
-  })
-  
-  var upload = multer({ storage: storage }).array('file')
-  
-app.get('/',function(req,res){
-    return res.send('Hello Server')
+  },
+  filename: function (req, file, cb) {
+
+    cb(null, file.originalname)
+  }
 })
-app.post('/upload',function(req, res) {
-    
-    upload(req, res, function (err) {
-     
-        if (err instanceof multer.MulterError) {
-            return res.status(500).json(err)
-          // A Multer error occurred when uploading.
-        } else if (err) {
-            return res.status(500).json(err)
-          // An unknown error occurred when uploading.
-        } 
-        
-        return res.status(200).send(req.file)
-        // Everything went fine.
+
+var upload = multer({ storage: storage }).any()
+app.get('/', function (req, res) {
+  return res.send('Hello Server')
+})
+app.post('/upload', async function (req, res) {
+
+  upload(req, res, async function (err) {
+
+    if (err instanceof multer.MulterError) {
+      return res.status(500).json(err)
+      // A Multer error occurred when uploading.
+    } else if (err) {
+      return res.status(500).json(err)
+      // An unknown error occurred when uploading.
+    }
+
+    console.log(images, audio);
+    const sortedImages = _.orderBy(images, ['path'], ['asc']); // Use Lodash to sort array by 'name'
+    console.log(sortedImages, 'sortedImages');
+    var videoOptions = {
+      fps: 25,
+      loop: 5, // seconds
+      transition: true,
+      transitionDuration: .5, // seconds
+      videoBitrate: 1024,
+      videoCodec: 'libx264',
+      size: '640x?',
+      audioBitrate: '128k',
+      audioChannels: 2,
+      format: 'mp4',
+      pixelFormat: 'yuv420p'
+    }
+
+    videoshow(sortedImages, videoOptions)
+      .audio('./public/audio/1.mp3')
+      .audioParams('./public/audio/2.mp3')
+      .save('./public/1slideshow.mp4')
+      .on('start', function (command) {
+        console.log('ffmpeg process started:', command)
       })
+      .on('error', function (err, stdout, stderr) {
+        console.error('Error:', err)
+        console.error('ffmpeg stderr:', stderr)
+      })
+      .on('end', function (output) {
+        console.error('Video created in:', output)
+      })
+    return res.status(200).send(req.images)
+
+    // Everything went fine.
+  })
 });
 
-app.listen(8000, function() {
-    console.log('App running on port 8000');
+app.listen(8000, function () {
+  console.log('App running on port 8000');
 });
--- src/App.js
+++ src/App.js
@@ -1,84 +1,106 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {Progress} from 'reactstrap';
+import { Progress } from 'reactstrap';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 class App extends Component {
   constructor(props) {
     super(props);
-      this.state = {
-        selectedFile: null,
-        loaded:0
-      }
-   
+    this.state = {
+      selectedImageFile: null,
+      selectedAudioFile: null,
+      surveyId: null,
+      loadedImages: 0,
+      loadedAudio: 0
+    }
+
   }
-  checkMimeType=(event)=>{
+  checkMimeType = (event) => {
     //getting file object
-    let files = event.target.files 
+    let files = event.target.files
     //define message container
     let err = []
     // list allow mime type
-   const types = ['image/png', 'image/jpeg', 'image/gif']
+    const types = ['image/png', 'image/jpeg', 'image/gif']
     // loop access array
-    for(var x = 0; x<files.length; x++) {
-     // compare file type find doesn't matach
-         if (types.every(type => files[x].type !== type)) {
-         // create error message and assign to container   
-         err[x] = files[x].type+' is not a supported format\n';
-       }
-     };
-     for(var z = 0; z<err.length; z++) {// if message not same old that mean has error 
-         // discard selected file
-        toast.error(err[z])
-        event.target.value = null
-    }
-   return true;
-  }
-  maxSelectFile=(event)=>{
-    let files = event.target.files
-        if (files.length > 3) { 
-           const msg = 'Only 3 images can be uploaded at a time'
-           event.target.value = null
-           toast.warn(msg)
-           return false;
+    for (var x = 0; x < files.length; x++) {
+      // compare file type find doesn't matach
+      if (types.every(type => files[x].type !== type)) {
+        // create error message and assign to container   
+        err[x] = files[x].type + ' is not a supported format\n';
       }
+    };
+    for (var z = 0; z < err.length; z++) {// if message not same old that mean has error 
+      // discard selected file
+      toast.error(err[z])
+      event.target.value = null
+    }
     return true;
- }
- checkFileSize=(event)=>{
-  let files = event.target.files
-  let size = 2000000 
-  let err = []; 
-  for(var x = 0; x<files.length; x++) {
-  if (files[x].size > size) {
-   err[x] = files[x].type+'is too large, please pick a smaller file\n';
- }
-};
-for(var z = 0; z<err.length; z++) {// if message not same old that mean has error 
-  // discard selected file
- toast.error(err[z])
- event.target.value = null
-}
-return true;
-}
-onChangeHandler=event=>{
-  var files = event.target.files
-  if(this.maxSelectFile(event) && this.checkMimeType(event) &&    this.checkFileSize(event)){ 
-  // if return true allow to setState
-     this.setState({
-     selectedFile: files,
-     loaded:0
-  })
-}
-}
+  }
+  maxSelectFile = (event) => {
+    let files = event.target.files
+    if (files.length > 3) {
+      const msg = 'Only 3 images can be uploaded at a time'
+      event.target.value = null
+      toast.warn(msg)
+      return false;
+    }
+    return true;
+  }
+  checkFileSize = (event) => {
+    let files = event.target.files
+    let size = 2000000
+    let err = [];
+    for (var x = 0; x < files.length; x++) {
+      if (files[x].size > size) {
+        err[x] = files[x].type + 'is too large, please pick a smaller file\n';
+      }
+    };
+    for (var z = 0; z < err.length; z++) {// if message not same old that mean has error 
+      // discard selected file
+      toast.error(err[z])
+      event.target.value = null
+    }
+    return true;
+  }
+  onChangeHandler = event => {
+    var files = event.target.files
+    // if (this.maxSelectFile(event) && this.checkMimeType(event) && this.checkFileSize(event)) {
+    // if return true allow to setState
+    this.setState({
+      selectedImageFile: files,
+      loadedImages: 0,
+    })
+    // }
+  }
+  onChangeHandlerAudio = event => {
+    var files = event.target.files
+    this.setState({
+      selectedAudioFile: files,
+      loadedAudio: 0
+    })
+  }
+  onChangeHandlerSurveyNo = event => {
+    var surveyId = event.target.value
+    this.setState({
+      surveyId: surveyId
+    })
+  }
   onClickHandler = () => {
-    const data = new FormData() 
-    for(var x = 0; x<this.state.selectedFile.length; x++) {
-      data.append('file', this.state.selectedFile[x])
+    const data = new FormData()
+    data.append('surveyId', this.state.surveyId)
+    for (var x = 0; x < this.state.selectedImageFile.length; x++) {
+      data.append('images', this.state.selectedImageFile[x])
+    }
+    for (var y = 0; y < this.state.selectedAudioFile.length; y++) {
+      data.append('audio', this.state.selectedAudioFile[y])
     }
     axios.post("http://localhost:8000/upload", data, {
       onUploadProgress: ProgressEvent => {
         this.setState({
-          loaded: (ProgressEvent.loaded / ProgressEvent.total*100),
+          loadedImages: (ProgressEvent.loaded / ProgressEvent.total * 100),
+          loadedAudio: (ProgressEvent.loaded / ProgressEvent.total * 100),
+
         })
       },
     })
@@ -88,27 +110,39 @@ onChangeHandler=event=>{
       .catch(err => { // then print response status
         toast.error('upload fail')
       })
-    }
+  }
 
   render() {
     return (
       <div class="container">
-	      <div class="row">
-      	  <div class="offset-md-3 col-md-6">
-               <div class="form-group files">
-                <label>Upload Your File </label>
-                <input type="file" class="form-control" multiple onChange={this.onChangeHandler}/>
-              </div>  
-              <div class="form-group">
+        <div class="row">
+          <div class="offset-md-3 col-md-6">
+            <div class="form-group">
+              <label>Survey Id </label>
+              <input type="text" class="form-control" onChange={this.onChangeHandlerSurveyNo} />
+            </div>
+            <div class="form-group files">
+              <label>Upload Your Images </label>
+              <input type="file" class="form-control" multiple onChange={this.onChangeHandler} />
+            </div>
+            <div class="form-group">
               <ToastContainer />
-              <Progress max="100" color="success" value={this.state.loaded} >{Math.round(this.state.loaded,2) }%</Progress>
-        
-              </div> 
-              
-              <button type="button" class="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button>
+              <Progress max="100" color="success" value={this.state.loadedImages} >{Math.round(this.state.loadedImages, 2)}%</Progress>
+            </div>
+            <div class="form-group files">
+              <label>Upload Your Audio </label>
+              <input type="file" class="form-control" multiple onChange={this.onChangeHandlerAudio} />
+            </div>
+            <div class="form-group">
+              <ToastContainer />
+              <Progress max="100" color="success" value={this.state.loadedAudio} >{Math.round(this.state.loadedAudio, 2)}%</Progress>
+            </div>
 
-	      </div>
-      </div>
+
+            <button type="button" class="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button>
+
+          </div>
+        </div>
       </div>
     );
   }
